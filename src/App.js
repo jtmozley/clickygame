cd32de8091b1310db312832cf0222a765ded2f50
Justin Mozley <jtmozley@gmail.com>
2019-06-11 05:07:43 -0500

    Code Clean Up
    Cleaned up the code on app.js by removing all commented code that wasn't needed for functionality of the game

--- src/App.js
+++ src/App.js
@@ -78,37 +78,3 @@ class App extends Component {
   }
 }
 export default App;
-
-//  // Filter this.state.friends for friends with an id not equal to the id being removed
-//  console.log("click worked");
-//  const myFriend = this.state.tempFriend.find(myId => myId.id === id);
-
-//  //if we havea copy check if score is high score and restart game
-//  //if the id is not found that means we arealdy pulled it out of the tempFriends list
-//  if (myFriend === undefined) {
-//    this.setState({
-//      //update state to game over
-//      //check top score
-//      //if topscore<score then topscore:this.state.score
-//      score: 0,
-//      msg: "Game Over!",
-//      tempFriend: this.state.friends
-//    });
-//  } else {
-//    //if there are no new matches go ahead and update the scores msgs,
-//    //then we need to score the value to check the current list of friends to the oldlist
-//    const oldListoffriends = this.state.tempFriend.filter(
-//      myId => myId.id !== id
-//    );
-//    this.setState({
-//      msg: "",
-//      score: this.state.score + 1,
-//      friends: this.state.friends,
-//      tempFriend: oldListoffriends
-//    });
-//    //update the state of tempFriend:oldListoffriends
-//  }
-
-//  const friends = this.state.friends.filter(friend => friend.id !== id);
-//  // Set this.state.friends equal to the new friends array
-//  this.setState({ friends });
