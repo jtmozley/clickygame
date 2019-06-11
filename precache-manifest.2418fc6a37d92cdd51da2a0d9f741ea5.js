4411ab97ef93aa6886d6c718e14e20b1baffd664
Justin Mozley <jtmozley@gmail.com>
2019-06-10 20:39:38 -0500

    Updates

---
+++ precache-manifest.2418fc6a37d92cdd51da2a0d9f741ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9b7afe902b4dafb747b8c6d9786dcb",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "ee6aa41f92fb0fa54209",
+    "url": "/clickygame/static/css/main.cb20d6ba.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ee6aa41f92fb0fa54209",
+    "url": "/clickygame/static/js/main.4b4a25d8.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
